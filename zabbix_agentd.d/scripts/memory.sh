c940ad8cde6000825f207fcffe019578bd90da3f
hongcoo <hongcoo@qq.com>
2018-04-02 14:59:53 +0800

    123

--- zabbix_agentd.d/scripts/memory.sh
+++ zabbix_agentd.d/scripts/memory.sh
@@ -6,5 +6,13 @@ case $KEY in
     'total')
         echo `free -b | grep "Mem:" | awk '{print $2}'`
         ;;
-
+    'used')
+        echo `free -b | grep "Mem:" | awk '{print $3}'`
+        ;;
+    'free')
+        echo `free -b | grep "Mem:" | awk '{print $4}'`
+        ;;
+    'available')
+        echo `free -b | grep "Mem:" | awk '{print $7}'`
+        ;;
 esac
